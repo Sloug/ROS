2fc12903409720b80f2af1b01c865cea9069e1eb
Sloug <carrylol@aol.com>
2021-05-10 10:21:54 +0200

    Aufgabe 3

--- husky_highlevel_controller/src/husky_highlevel_controller_node.cpp
+++ husky_highlevel_controller/src/husky_highlevel_controller_node.cpp
@@ -1,55 +1,13 @@
-#include "ros/ros.h"
-#include "sensor_msgs/LaserScan.h"
-#include "limits"
+#include <ros/ros.h>
+#include "husky_highlevel_controller/HuskyHighlevelController.hpp"
 
-ros::Publisher publisher;
-double last_min;
-void scannerCallback(const sensor_msgs::LaserScan& msg)
+int main(int argc, char** argv)
 {
-	for(float f:msg.ranges)
-	{
-		if(f< last_min)
-		{
-			last_min = f;
-		}
-	}
-	
-	//publisher.publish(msg);
-	ROS_INFO("I heard: [%11.5f]", last_min);
-}
+    ros::init(argc, argv, "husky_highlevel_controller");
+    ros::NodeHandle nh("~");
 
+    husky_highlevel_controller::HuskyHighlevelController hhlc(nh);
+    hhlc.listen();
 
-
-int main(int argc, char **argv)
-{
-	ros::init(argc, argv, "listener");
-	ros::NodeHandle nodeHandle;
-	last_min = std::numeric_limits<float>::infinity();
-	//load params
-	std::string topic_name;
-	if(!nodeHandle.getParam("scan/topic_name", topic_name))
-	{
-		ROS_ERROR("Could not find topic parameter1!");
-		ros::requestShutdown();
-		return 1;
-	}
-	int queue_size;
-	if(!nodeHandle.getParam("scan/queue_size", queue_size))
-	{
-		ROS_ERROR("Could not find topic parameter2!");
-		ros::requestShutdown();
-		return 1;
-	}
-	
-	publisher =
-		nodeHandle.advertise<sensor_msgs::LaserScan>("laser_scan",
-		queue_size);
-
-	
-	ros::Subscriber subscriber =
-		nodeHandle.subscribe(topic_name, queue_size, scannerCallback);
-
-	
-	ros::spin();
-	return 0;
+    return 0;
 }
